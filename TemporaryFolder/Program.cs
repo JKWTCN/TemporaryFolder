6d5ef724c42c32bf1089bcb6c5ada5c5e2de9d15
JKWTCN <jkwtcn@icloud.com>
2025-03-04 01:28:10 +0800

    添加项目文件。

---
+++ TemporaryFolder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace TemporaryFolder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tmp_dir = "D:\\Temporary";
+            if (!Directory.Exists(tmp_dir))
+            {
+                Directory.CreateDirectory(tmp_dir);
+            }
+            DateTime now = DateTime.Now;
+            string fold_1 = now.ToString("yyMM");
+            fold_1 = $"{tmp_dir}\\{fold_1}";
+            if (!Directory.Exists(fold_1))
+            {
+                Directory.CreateDirectory(fold_1);
+            }
+            string fold_2 = now.ToString("dd");
+            fold_2 = $"{fold_1}\\{fold_2}";
+            if (!Directory.Exists(fold_2))
+            {
+                Directory.CreateDirectory(fold_2);
+            }
+            Process.Start("explorer.exe", fold_2);
+        }
+    }
+}
